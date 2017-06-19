a1734b6ec78c89119c961b8d07db1c2e15817caf
MichalGrzesiak <mgrzesiak147@gmail.com>
2017-06-19 20:13:50 +0200

    synch

--- FEC_RepetitionCode.m
+++ FEC_RepetitionCode.m
@@ -7,12 +7,12 @@ sygnal=sygnal';
 %gen_repetitionCode();
 %gen_fromWav();
 %generowanie2();
- sygnal=encode_read_salomon(sygnal,3,7);
+ sygnal=encode_read_salomon(sygnal,3,15);
 %sygnal=encode_hammingCode(p,sygnal);
 %n=encode_repetitionCode();
 
 sygnal=gen_basic_mistakeBits(b,sygnal);
- sygnal=decode_read_salomon(sygnal,3,7);
+ sygnal=decode_read_salomon(sygnal,3,15);
 
 %gen_gilbert_mistake(); 
 %sygnal=decode_hammingCode(p,sygnal)
