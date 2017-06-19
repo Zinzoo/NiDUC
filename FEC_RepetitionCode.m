e5b2bdf23b647627447e2f48e961871d583e6087
MichalGrzesiak <mgrzesiak147@gmail.com>
2017-06-19 18:15:46 +0200

    READ-SALOMON DZIALA

--- FEC_RepetitionCode.m
+++ FEC_RepetitionCode.m
@@ -2,17 +2,20 @@ clear all;
 
 p=11; %
 b=0.1;
-sygnal=[1;1;1;1;1;1;1;1;1;1;1;1;1;1;1];
+sygnal=[1;1;1;1;1;1];
+sygnal=sygnal';
 %gen_repetitionCode();
 %gen_fromWav();
 %generowanie2();
-
-sygnal=encode_hammingCode(p,sygnal);
+ sygnal=encode_read_salomon(sygnal,3,7);
+%sygnal=encode_hammingCode(p,sygnal);
 %n=encode_repetitionCode();
 
-%sygnal=gen_basic_mistakeBits(b,sygnal);
+sygnal=gen_basic_mistakeBits(b,sygnal);
+ sygnal=decode_read_salomon(sygnal,3,7);
+
 %gen_gilbert_mistake(); 
-sygnal=decode_hammingCode(p,sygnal)
+%sygnal=decode_hammingCode(p,sygnal)
 sygnal
 %decode_repetitionCode();
 
---
+++ encode_read_salomon.m
@@ -0,0 +1,51 @@
+function [ out ] = encode_read_salomon(sygnal, k,n )
+%
+%sygnal=[1,2,3,4,5,6,1,2,3,4,5,6]; %% bity sygnalu musz¹ byæ parzyste  // musz¹ dzieliæ 
+%% siê na 2k
+
+%n=8;
+m=log2(n+1);
+
+
+
+%m = 3;           % Number of bits per symbol m>=3
+%n = 2^m - 1;     % Codeword length
+%k = 3;           % Message length // k musi byæ nieparzyste
+                    %N i K musz¹ ró¿niæ siê dodatni¹ liczb¹ ca³kowit¹.
+                    % K<M
+                    final=[];
+                    y=[];
+q=0;              
+  iteracja=ceil(length(sygnal)/(2*k));
+        
+          for ( i= 1 : iteracja)
+                    s1=sygnal(q+1:q+k);    
+                    s2=sygnal(q+k+1:q+2*k);
+                    s3=[s1;s2]
+                    msg = gf(s3,m)
+                    code = rsenc(msg,n,k)
+          
+                    x=code (1:1 ,1:n)
+                                % przekszta³cenie z Galois  na zwyk³e
+                              for ( i= 1 : n) % nie wiem czy petla potrzebna
+                                  myvar_dbl = double( x.x ); %jakaœ chora magia z neta od hindusów
+                              end
+                              %% nie dotykam tego na górze wa¿ne ¿e dzia³a
+                              
+                    final=[final myvar_dbl]
+                    x=code (2:2 ,1:n)
+                     % przekszta³cenie z Galois  na zwyk³e
+                              for ( i= 1 : n) % nie wiem czy petla potrzebna
+                                  myvar_dbl = double( x.x ); %jakaœ chora magia z neta od hindusów
+                              end
+                              %% nie dotykam tego na górze wa¿ne ¿e dzia³a
+                    
+                    final=[final myvar_dbl]
+                    
+                    q=q+2*k;
+          end 
+                    
+                    out = final;
+
+end
+
